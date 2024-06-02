26c3daf511b04b8b9d1dd87d86dd957fd6ba45a6
Bagus2004 <bagusardiansyah614@gmail.com>
2024-06-03 03:54:53 +0700

    update in ci

--- project-root/app/Views/books/edit.php
+++ project-root/app/Views/books/edit.php
@@ -4,14 +4,19 @@
 <div class="container">
     <div class="row">
         <div class="col-8">
-            <h2 class="my-3">Form Tambah Data Buku</h2>
-            <form action="/books/save" method="post" enctype="multipart/form-data">
+            <h2 class="my-3">Form Ubah Data Buku</h2>
+            <form action="/books/update/<?= $buku['id']; ?>" method="post" enctype="multipart/form-data">
                 <?= csrf_field(); ?>
+                <input type="hidden" name="slug" value="<?= $buku['slug']; ?>">
+                <input type="hidden" name="sampulLama" value="<?= $buku['sampul']; ?>">
+
                 <div class="row mb-3">
                     <label for="judul" class="col-sm-2 col-form-label">Judul</label>
                     <div class="col-sm-10">
-                        <input type="judul" class="form-control <?= ($validation->hasError('judul')) ? 'is-invalid' :
-                            ''; ?>" id="judul" name="judul" value="<?= old('judul'); ?>">
+                        <input type="judul"
+                            class="form-control <?= ($validation->hasError('judul')) ? 'is-invalid' : ''; ?>" id="judul"
+                            name="judul" autofocus="autofocus"
+                            value="<?= (old('judul')) ? old('judul') : $buku['judul'] ?>">
                         <div class="invalid-feedback">
                             <?= $validation->getError('judul'); ?>
                         </div>
@@ -21,7 +26,8 @@
                     <label for="penulis" class="col-sm-2 col-form-label">Penulis</label>
                     <div class="col-sm-10">
                         <input type="penulis" class="form-control <?= ($validation->hasError('penulis')) ? 'is-invalid' :
-                            ''; ?>" id="penulis" name="penulis" value="<?= old('penulis'); ?>">
+                            ''; ?>" id="penulis" name="penulis" autofocus="autofocus"
+                            value="<?= (old('penulis')) ? old('penulis') : $buku['penulis'] ?>">
                         <div class="invalid-feedback">
                             <?= $validation->getError('penulis'); ?>
                         </div>
@@ -31,17 +37,18 @@
                     <label for="penerbit" class="col-sm-2 col-form-label">Penerbit</label>
                     <div class="col-sm-10">
                         <input type="penerbit" class="form-control <?= ($validation->hasError('penerbit')) ? 'is-invalid' :
-                            ''; ?>" id="penerbit" name="penerbit" value="<?=
-                             old('penerbit'); ?>">
+                            ''; ?>" id="penerbit" name="penerbit" autofocus="autofocus"
+                            value="<?= (old('penerbit')) ? old('penerbit') : $buku['penerbit'] ?>">
                         <div class="invalid-feedback">
                             <?= $validation->getError('penerbit'); ?>
                         </div>
                     </div>
                 </div>
+
                 <div class="input-group mb-3">
                 <label for="sampul" class="col-sm-2 col-form-label">Sampul</label>
                     <div class="col-sm-2">
-                        <img src="/img/no-cover.jpg" class="img-thumbnail img-preview">
+                        <img src="/img/<?= $buku['sampul']; ?>" class="img-thumbnail img-preview">
                     </div>
                     <div class="col-sm-8">
                         <input type="file" class="form-control <?= ($validation->hasError('sampul')) ? 'is-invalid' :
@@ -52,9 +59,9 @@
                         <label for="sampul" class="input-group-text" >Upload</label>
                     </div>
                 </div>
-                <button type="submit" class="btn btn-primary">Tambah Data</button>
+                    <button type="submit" class="btn btn-primary">Ubah Data</button>   
         </div>
-        </form>
+            </form>
     </div>
 </div>
 <?= $this->endSection(); ?>
